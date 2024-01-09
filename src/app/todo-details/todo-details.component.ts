b1463e835288e4d0500da67838642e71273c7510
PROD\mbahmanyar <masoud.bahmanyar@alten.it>
2024-01-09 09:36:09 +0100

    Pipe Async replaced

--- src/app/todo-details/todo-details.component.ts
+++ src/app/todo-details/todo-details.component.ts
@@ -1,45 +1,26 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Todo } from '../todo/interfaces/todo.interface';
 import { TodoService } from '../todo/services/todo.service';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { interval, map, Observable, of, Subject, Subscription, switchMap, takeUntil } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { catchError, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-todo-details',
   templateUrl: './todo-details.component.html',
   styleUrls: ['./todo-details.component.css']
 })
-export class TodoDetailsComponent implements OnInit, OnDestroy {
-  todoSelected: Todo;
+export class TodoDetailsComponent implements OnInit {
   todo$: Observable<Todo>;
-  todoId: number;
-  counter = interval(1000);
 
-  todoservice: TodoService = inject(TodoService);
-  activeRoute: ActivatedRoute = inject(ActivatedRoute);
-
-  private destroy$: Subject<void> = new Subject();
+  constructor(private todoservice: TodoService, private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.todoId = this.activeRoute.snapshot.params['id'];
-    const subscription = this.todoservice.getOne(this.todoId).pipe(
-      switchMap((todo) => {
-        this.todoSelected = todo;
-        const counterSubscription =this.counter.subscribe((val) => { console.log(val) });
-        this.destroy$.subscribe(() => counterSubscription.unsubscribe());
-        return of(todo);
-      }),
-      takeUntil(this.destroy$)
-    ).subscribe(() => {
-      
-      console.log('Subscribed');
-    })
+    const todoId = this.activeRoute.snapshot.params['id'];
+    this.todo$ = this.todoservice.getOne(todoId).pipe(
+      catchError(error => {
+        console.error(error);
+        return of(null as unknown as Todo);
+      })
+    );
   }
-  
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-    console.log('Component destroyed and unsubscribed');
-  }
-
 }
